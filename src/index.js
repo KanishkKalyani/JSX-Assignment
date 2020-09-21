3b36274e857318968029869e05baa6a2dcf4e465
KanishkKalyani <you@example.comkanishkkalyani@gmail.com>
2020-09-21 14:49:12 +0530

    First Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import * as serviceWorker from "./serviceWorker";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const state = {
+	type: "div",
+	name: "Clock",
+	root: true,
+	style: {
+		display: "flex",
+		"flex-direction": "row",
+		"justify-content": "center",
+	},
+	children: [
+		{
+			type: "div",
+			name: "Hour",
+			style: {},
+			children: [],
+		},
+		{
+			type: "span",
+			name: "Minute",
+			style: {
+				color: "green",
+				"font-size": 30,
+			},
+			children: [],
+		},
+	],
+};
+
+console.log(state.style);
+
+const Component1 = React.createElement(
+	state.type,
+	{ style: state.style },
+	state.name,
+	state.children.map((child, index) =>
+		React.createElement(child.type, { style: child.style }, child.name)
+	)
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render(Component1, document.getElementById("root"));
+
 serviceWorker.unregister();
